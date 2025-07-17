39484c1298633ff76c86ff1be529725b093655e9
abosler15 <47117445+abosler15@users.noreply.github.com>
2025-07-17 15:06:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,15 @@ provider "azurerm" {
   subscription_id = "d52a6d4f-8a6b-4e23-a011-dbb0c8d7eddc" 
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "my-rg-amy"
+    storage_account_name = "mytfstateacctamy"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
 resource "azurerm_resource_group" "rg" {
   name = "ab-basic-pipeline"
   location = "westus2"
